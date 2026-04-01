d082c4399976187dd932a90986e2e493256b17f4
Nick Tapp-Hughes <nicholas.tapphughes@gmail.com>
2026-03-31 20:58:25 -0400

    Restore cursor position to last known position when opening buffer

--- init.lua
+++ init.lua
@@ -553,3 +553,13 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- Restore cursor position when reopening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local line_count = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= line_count then
+      vim.api.nvim_win_set_cursor(0, mark)
+    end
+  end,
+})
